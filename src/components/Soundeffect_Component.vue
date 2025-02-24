b5ccc709222389f8557d1e6c8ea4148c50544a30
mbauerTgm <mbauer@student.tgm.ac.at>
2025-02-24 15:24:31 +0100

    24.02.2025 15:24

--- src/components/Soundeffect_Component.vue
+++ src/components/Soundeffect_Component.vue
@@ -23,7 +23,7 @@
             </v-container>
           </v-col>
           <v-col>
-            <v-text-field class="ml-n16" rounded="lg" variant="outlined"></v-text-field>
+            <v-text-field v-model="name" class="ml-n16" rounded="lg" variant="outlined"></v-text-field>
           </v-col>
         </v-row>
         <v-row class="mb-n8 mr-4">
@@ -34,15 +34,16 @@
           </v-col>
           <v-col>
             <v-file-input
-              class="ml-n16"
-              rounded="lg"
-              variant="outlined"
-              accept=".mp3"
-              label="Drag & Drop or Click"
-              show-size
-              prepend-icon=""
-              append-inner-icon="mdi-music"
-            ></v-file-input>          
+            class="ml-n16"
+            rounded="lg"
+            variant="outlined"
+            accept=".mp3"
+            label="Drag & Drop or Click"
+            show-size
+            prepend-icon=""
+            append-inner-icon="mdi-music"
+            @change="handleFileUpload"
+          ></v-file-input>            
           </v-col>
         </v-row>
         <v-row>
@@ -59,43 +60,63 @@
   </template>
   
   <script>
-    import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        name: '',
-        country_code: '',
-        country_name: '',
-        flag: ''
-      };
-    },
-    methods: {
-      async createSound() {
-        try {
-          const response = await axios.post('http://localhost:5000/setTable',{
-           table: 'soundeffect',
-           data:{
-            name: this.name,
-            flag: this.flag
-           }
-          });
+    import axios from "axios";
 
-          console.log('Team erfolgreich erstellt:', response.data);
-          alert('Team wurde erfolgreich erstellt!');
-        } catch (error) {
-          console.error('Fehler beim Erstellen der Soundeffect:', error.response?.data || error.message);
-        }finally{
-          this.resetForm();
-          this.$forceUpdate();
-        }
-      },
-      resetForm() {
-        this.name = '';
-        this.flag = '';
+export default {
+  data() {
+    return {
+      name: "",
+      file: null
+    };
+  },
+  methods: {
+    handleFileUpload(event) {
+    const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          this.file = reader.result.split(',')[1]; // Extract Base64 data
+        };
       }
+    },
+    async createSound() {
+  try {
+    // Debugging log to check what we are sending
+    console.log("Sending Data:", { name: this.name, file: this.file });
+
+    // Ensure that the file is in Base64 format (should not be an object)
+    if (!this.name || !this.file) {
+      alert("Please provide both a name and a valid file.");
+      return;
     }
-  };  
+
+    const response = await axios.post("http://localhost:5000/setTable", {
+      table: "soundeffect",
+      data: {
+        name: this.name,
+        file: this.file, // Send Base64 string
+      },
+    });
+
+    console.log("Soundeffect erfolgreich erstellt:", response.data);
+    alert("Soundeffect wurde erfolgreich erstellt!");
+  } catch (error) {
+    console.error("Fehler beim Erstellen der Soundeffect:", error.response?.data || error.message);
+  } finally {
+    this.resetForm();
+    this.$forceUpdate();
+  }
+},
+
+
+resetForm() {
+  console.log("Resetting form...");
+  this.name = "";
+  this.file = null;
+}
+  },
+};
   </script>
 
   <style scoped>
