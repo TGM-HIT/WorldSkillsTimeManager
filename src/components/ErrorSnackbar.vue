d1e3f4eddc1f6bc03f40e1d8b3616b538a94e991
viktor Bublinksyyy <vbublinskyy@student.tgm.ac.at>
2025-03-05 14:40:29 +0100

    05.03.2025 14:40

---
+++ src/components/ErrorSnackbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-snackbar 
+      :model-value="show" 
+      @update:model-value="updateShow"
+      location="bottom right" 
+      color="error" 
+      timeout="3000"
+    >
+      <v-icon class="mr-2">$error</v-icon>
+      <span>error thrown</span>
+    </v-snackbar>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      show: Boolean,
+    },
+    emits: ["update:show"],
+    methods: {
+      updateShow(value) {
+        this.$emit("update:show", value);
+      },
+    },
+  };
+  </script>
+  
