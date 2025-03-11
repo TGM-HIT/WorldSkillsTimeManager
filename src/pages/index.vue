f97853395f8def97123c35a96ed6e506aae8fd09
mbauerTgm <mbauer@student.tgm.ac.at>
2025-03-11 08:11:04 +0100

    11.03.2025 08:00

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-  <!--
     <br>
     <br>
     <br>
     <h1 style="text-align: center;">Startseite</h1>
     <h1 style="text-align: center;">(Platzhalter)</h1>
-    
+    <!--
     <Home />
-  -->
+  
     <Babler_Component />
+    -->
 
   </div>
 
