88591d7ba5b392d0d281ef72dbded7d8126faa43
mbauerTgm <mbauer@student.tgm.ac.at>
2025-04-01 11:30:46 +0200

    01.04.2025 11:30

--- src/components/Table_Component.vue
+++ src/components/Table_Component.vue
@@ -129,11 +129,15 @@ export default {
     async getResourcesAndTimeslots() {
       try {
         const resources = await axios.get('http://localhost:5000/getTable?tablename=timeslot_resources');
+        const timeslot_types = await axios.get('http://localhost:5000/getTable?tablename=timeslot_types');
+
         const newResources = [];
         for (let i = 0; i < resources.data.length; i++){
           const response2 = await axios.get('http://localhost:5000/getRow?tablename=timeslot&id=' + resources.data[i].timeslot_id);
           const response3 = await axios.get('http://localhost:5000/getRow?tablename=resource&id=' + resources.data[i].resource_id);
-         // newResources.push({id: resources.data[i].timeslot_id, resourceId: response4.data[0].});
+          const response4 = await axios.get('http://localhost:5000/getRow?tablename=timeslot_teams&condition=timeslot_id=' + resources.data[i].timeslot_id);
+
+         // newResources.push({id: resources.data[i].timeslot_id, resourceId: response4.data[0].team_id, title:});
         }
       } catch (error) {
         console.error("Fehler beim Abrufen der Daten:", error);
