05c477168ca279a1c5472929bfe55b06dd24792c
viktor Bublinksyyy <vbublinskyy@student.tgm.ac.at>
2025-06-11 12:54:01 +0200

    11.06.2025 12:55

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,6 +6,7 @@ const port = 5000;
 const portweb = 5001;
 const SECRET_KEY = "6LciXfkqAAAAAIV_RYSNfdPpjjozwLFhGgo3DpUj";
 
+app.use(cors());
 app.use(express.json({ limit: "15mb" }));
 
 // GET request to fetch a table
@@ -185,9 +186,14 @@ app.post("/setTimeslot", async (req, res) => {
     try {
         await functions.setTimeslot(timeslot, (err, result) => {
             if (err) {
-                console.error("Error saving timeslot:", err);
-                return res.status(500).json({ error: "Failed to save timeslot" });
-            }
+                    console.error("Error saving timeslot:", err);
+    
+                    // Wenn der Fehler ein strukturiertes Objekt mit .message ist:
+                    const message = err.message || "Unknown error";
+                    const statusCode = err.code === "OVERLAP_ERROR" ? 400 : 500;
+
+                    return res.status(statusCode).json({ message });
+                    }
             res.status(201).json(result);
         });
     } catch (error) {
@@ -201,9 +207,11 @@ app.post("/editTimeslot", async (req, res) => {
     try {
         await functions.editTimeslot(timeslot, (err, result) => {
             if (err) {
-                console.error("Error editing timeslot:", err);
-                return res.status(500).json({ error: "Failed to edit timeslot" });
-            }
+                    console.error("Error editing timeslot:", err);
+                    const message = err.message || "Unknown error";
+                    const statusCode = err.code === "OVERLAP_ERROR" ? 400 : 500;
+                    return res.status(statusCode).json({ message });
+                    }
             res.status(200).json(result);
         });
     } catch (error) {
--- src/components/Timeslot_Component.vue
+++ src/components/Timeslot_Component.vue
@@ -315,6 +315,7 @@ export default {
           this.showSuccess = true;
           setTimeout(() => (this.showSuccess = false), 3000);
         } catch (error) {
+           this.errorMessage = error.response?.data?.message || 'Fehler beim Erstellen des Timeslots';
           this.showError = true;
           console.log(error);
           setTimeout(() => (this.showError = false), 3000);
@@ -421,7 +422,7 @@ export default {
           setTimeout(() => (this.showSuccess = false), 3000);
           this.returnToList();
         } catch (error) {
-          this.errorMessage = error.response?.data?.message || 'Fehler beim Erstellen des Timeslots';
+          this.errorMessage = error.response?.data?.message || 'Fehler beim Bearbeiten des Timeslots';
           this.showError = true;
           setTimeout(() => (this.showError = false), 3000);
         } finally {
